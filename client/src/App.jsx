2c2a5946cbda869f29984e426d6a986f88ff2977
Mobeen Qasim <mobeenqq@gmail.com>
2025-05-03 05:10:33 -0400

    updated frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Hero from './sections/Hero';
 import Calendar from './sections/Calendar';
 import Login from './sections/Login';
+import Navbar from './sections/Navbar';
 
 const App = () => {
   return (
-    <main className="overflow-hidden">
-      <Navbar></Navbar>
-      <Hero />
-      <Calendar />
-      <Login />
-    </main>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          <main className="overflow-hidden">
+            <Hero />
+            {/* <Calendar /> */}
+          </main>
+        } />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
 };
 
