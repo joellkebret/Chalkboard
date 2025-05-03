f091ef1db84bde816be86de65e373bcf4381b720
Mobeen Qasim <mobeenqq@gmail.com>
2025-05-03 04:05:36 -0400

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from './sections/Hero';
 
 const App = () => {
   return (
     <main className='overflow-hidden'>
-      
+      <Hero/>
     </main>
   )
 }
---
+++ client/src/sections/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+   <section className='relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32'>
+      <div name='hero'>
+        <h1>hello</h1>
+        </div>
+   </section>
+  )
+}
+
+export default Hero
